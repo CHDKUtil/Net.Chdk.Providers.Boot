ea432909a237ac291a9cd8f954b52e1d1ea27196
Dmitry Shechtman <damnian@gmail.com>
2017-05-26 17:21:15 +0300

    Extract base BootProvider

--- src/Net.Chdk.Providers.Boot/BootProvider.cs
+++ src/Net.Chdk.Providers.Boot/BootProvider.cs
@@ -1,98 +1,26 @@
-﻿using Net.Chdk.Json;
-using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
+﻿using System.Linq;
 
 namespace Net.Chdk.Providers.Boot
 {
-    sealed class BootProvider : IBootProvider
+    sealed class BootProvider : BootProvider<BootProvider.BootData>, IBootProvider
     {
-        #region Constructor
-
-        public BootProvider()
-        {
-            bytes = new Lazy<Dictionary<string, Dictionary<int, byte[]>>>(GetBytes);
-        }
-
-        #endregion
-
         #region IBootProvider Members
 
-        public IDictionary<int, byte[]> GetBytes(string fileSystem)
-        {
-            Dictionary<int, byte[]> bytes;
-            Bytes.TryGetValue(fileSystem, out bytes);
-            return bytes;
-        }
-
-        public string FileName => Boot.FileName;
-        public int[][] Offsets => Boot.Offsets;
-        public byte[] Prefix => Boot.Prefix;
+        public string FileName => Data.Files.First().Key;
+        public int[][] Offsets => Data.Offsets;
+        public byte[] Prefix => Data.Prefix;
 
         #endregion
 
-        #region Boot
+        #region Data
 
-        private const string DataPath = "Data";
-        private const string BootDataFileName = "boot.json";
-
-        private sealed class BootData
+        internal sealed class BootData : DataBase
         {
-            public string FileName { get; set; }
-            public Dictionary<string, Dictionary<string, string>> Strings { get; set; }
             public int[][] Offsets { get; set; }
             public byte[] Prefix { get; set; }
         }
 
-        private readonly Lazy<BootData> boot = new Lazy<BootData>(GetBoot);
-
-        private BootData Boot => boot.Value;
-
-        private static BootData GetBoot()
-        {
-            var filePath = Path.Combine(DataPath, BootDataFileName);
-            using (var reader = File.OpenText(filePath))
-            using (var jsonReader = new JsonTextReader(reader))
-            {
-                return JsonSerializer.CreateDefault(Settings).Deserialize<BootData>(jsonReader);
-            }
-        }
-
-        private static JsonSerializerSettings Settings => new JsonSerializerSettings
-        {
-            Converters = new[] { new HexStringJsonConverter() }
-        };
-
-        #endregion
-
-        #region Bytes
-
-        private readonly Lazy<Dictionary<string, Dictionary<int, byte[]>>> bytes;
-
-        private Dictionary<string, Dictionary<int, byte[]>> Bytes => bytes.Value;
-
-        private Dictionary<string, Dictionary<int, byte[]>> GetBytes()
-        {
-            return Boot.Strings.ToDictionary(kvp => kvp.Key, GetBytes);
-        }
-
-        private static Dictionary<int, byte[]> GetBytes(KeyValuePair<string, Dictionary<string, string>> kvp)
-        {
-            return kvp.Value.ToDictionary(GetStartIndex, GetBytes);
-        }
-
-        private static int GetStartIndex(KeyValuePair<string, string> kvp)
-        {
-            return Convert.ToInt32(kvp.Key, 16);
-        }
-
-        private static byte[] GetBytes(KeyValuePair<string, string> kvp)
-        {
-            return Encoding.ASCII.GetBytes(kvp.Value);
-        }
+        protected override string DataFileName => "boot.json";
 
         #endregion
     }
---
+++ src/Net.Chdk.Providers.Boot/BootProviderBase.cs
@@ -0,0 +1,110 @@
+﻿using Net.Chdk.Json;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Net.Chdk.Providers.Boot
+{
+    abstract class BootProvider<TData>
+        where TData : BootProvider<TData>.DataBase
+    {
+        #region Constructor
+
+        protected BootProvider()
+        {
+            data = new Lazy<TData>(GetData);
+            bytes = new Lazy<Dictionary<string, Dictionary<int, byte[]>>>(GetBytes);
+            files = new Lazy<Dictionary<string, byte[]>>(DoGetFiles);
+        }
+
+        #endregion
+
+        public IDictionary<int, byte[]> GetBytes(string fileSystem)
+        {
+            Dictionary<int, byte[]> bytes;
+            Bytes.TryGetValue(fileSystem, out bytes);
+            return bytes;
+        }
+
+        #region Data
+
+        internal abstract class DataBase
+        {
+            public Dictionary<string, Dictionary<string, string>> Strings { get; set; }
+            public Dictionary<string, string> Files { get; set; }
+        }
+
+        private readonly Lazy<TData> data;
+
+        protected TData Data => data.Value;
+
+        protected virtual string DataPath => "Data";
+
+        protected abstract string DataFileName { get; }
+
+        private TData GetData()
+        {
+            var filePath = Path.Combine(DataPath, DataFileName);
+            using (var reader = File.OpenText(filePath))
+            using (var jsonReader = new JsonTextReader(reader))
+            {
+                return JsonSerializer.CreateDefault(Settings).Deserialize<TData>(jsonReader);
+            }
+        }
+
+        private static JsonSerializerSettings Settings => new JsonSerializerSettings
+        {
+            Converters = new[] { new HexStringJsonConverter() }
+        };
+
+        #endregion
+
+        #region Bytes
+
+        private readonly Lazy<Dictionary<string, Dictionary<int, byte[]>>> bytes;
+
+        private Dictionary<string, Dictionary<int, byte[]>> Bytes => bytes.Value;
+
+        private Dictionary<string, Dictionary<int, byte[]>> GetBytes()
+        {
+            return Data.Strings.ToDictionary(kvp => kvp.Key, GetBytes);
+        }
+
+        private static Dictionary<int, byte[]> GetBytes(KeyValuePair<string, Dictionary<string, string>> kvp)
+        {
+            return kvp.Value.ToDictionary(GetStartIndex, GetBytes);
+        }
+
+        #endregion
+
+        #region Files
+
+        private Lazy<Dictionary<string, byte[]>> files;
+
+        protected Dictionary<string, byte[]> Files => files.Value;
+
+        private Dictionary<string, byte[]> DoGetFiles()
+        {
+            return Data.Files.ToDictionary(kvp => kvp.Key, GetBytes);
+        }
+
+        #endregion
+
+        #region Helper Methods
+
+        private static int GetStartIndex(KeyValuePair<string, string> kvp)
+        {
+            return Convert.ToInt32(kvp.Key, 16);
+        }
+
+        private static byte[] GetBytes(KeyValuePair<string, string> kvp)
+        {
+            return Encoding.ASCII.GetBytes(kvp.Value);
+        }
+
+        #endregion
+    }
+}
